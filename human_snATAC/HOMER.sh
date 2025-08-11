b843d7d38b9d77830528b71cbe5290df28b315be
smilesw <72909079+smilesw@users.noreply.github.com>
2025-08-11 15:39:22 +0900

    Update HOMER.sh

--- human_snATAC/HOMER.sh
+++ human_snATAC/HOMER.sh
@@ -1,5 +1,5 @@
-findMotifsGenome.pl ./DAP_PEC1.bed hg38 ./ -size 200 -mask
-findMotifsGenome.pl ./DAP_PEC2.bed hg38 ./ -size 200 -mask
-findMotifsGenome.pl ./DAP_Prepodocyte.bed hg38 ./ -size 200 -mask
-findMotifsGenome.pl ./DAP_Podocyte1.bed hg38 ./ -size 200 -mask
-findMotifsGenome.pl ./DAP_Podocyte3.bed hg38 ./ -size 200 -mask
+findMotifsGenome.pl ./DAR_PEC1.bed hg38 ./ -size 200 -mask
+findMotifsGenome.pl ./DAR_PEC2.bed hg38 ./ -size 200 -mask
+findMotifsGenome.pl ./DAR_Prepodocyte.bed hg38 ./ -size 200 -mask
+findMotifsGenome.pl ./DAR_Podocyte1.bed hg38 ./ -size 200 -mask
+findMotifsGenome.pl ./DAR_Podocyte3.bed hg38 ./ -size 200 -mask
