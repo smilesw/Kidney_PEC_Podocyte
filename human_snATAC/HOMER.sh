f5145696fbbdd333594c26c451ba3421d124480c
smilesw <72909079+smilesw@users.noreply.github.com>
2025-08-11 15:38:33 +0900

    Create HOMER.sh

---
+++ human_snATAC/HOMER.sh
@@ -0,0 +1,5 @@
+findMotifsGenome.pl ./DAP_PEC1.bed hg38 ./ -size 200 -mask
+findMotifsGenome.pl ./DAP_PEC2.bed hg38 ./ -size 200 -mask
+findMotifsGenome.pl ./DAP_Prepodocyte.bed hg38 ./ -size 200 -mask
+findMotifsGenome.pl ./DAP_Podocyte1.bed hg38 ./ -size 200 -mask
+findMotifsGenome.pl ./DAP_Podocyte3.bed hg38 ./ -size 200 -mask
